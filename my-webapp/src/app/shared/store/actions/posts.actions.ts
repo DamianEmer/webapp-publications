3ea7d40394b54b9df296409f2445ff4d9f2d7786
Damian <emer_smart17@outlook.com>
2019-01-10 09:29:12 -0600

    Cambio de nombres en actions
    Nombres descriptivos para cada accion

--- my-webapp/src/app/shared/store/actions/posts.actions.ts
+++ my-webapp/src/app/shared/store/actions/posts.actions.ts
@@ -2,20 +2,28 @@ import { Action } from '@ngrx/store';
 import { Post } from 'src/app/home/models/post.interface';
 
 export enum ActionTypes {
-    LOADING_POSTS = '[Post] Load Posts',
-    LOAD_POSTS = '[Post] Loading Posts'
+    LOAD_POSTS = '[Post] Load Posts',
+    LOAD_POSTS_SUCCESS = '[Post] Load Posts Success',
+    LOAD_ERROR = '[Post] Load Error' 
 }
 
 export class LoadingPosts implements Action {
-    readonly type = ActionTypes.LOADING_POSTS;
+    readonly type = ActionTypes.LOAD_POSTS;
 }
 
 export class LoadPosts implements Action {
-    readonly type = ActionTypes.LOAD_POSTS;
+    readonly type = ActionTypes.LOAD_POSTS_SUCCESS;
 
     constructor(public payload: Post[]) { }
 }
 
+export class LoadError implements Action {
+    readonly type = ActionTypes.LOAD_ERROR
+
+    constructor ( public payload: string ) { }    
+}
+
 export type All_Actions = 
     |   LoadingPosts
-    |   LoadPosts;
+    |   LoadPosts
+    |   LoadError;
--- my-webapp/src/app/shared/store/reducers/posts.reducer.ts
+++ my-webapp/src/app/shared/store/reducers/posts.reducer.ts
@@ -3,26 +3,34 @@ import { Post } from '../../../home/models/post.interface';
 
 export interface State {
     isLoading: boolean;
-    posts: Post[]
+    posts: Post[],
+    error_msg: string
 }
 
 export const initialState: State = {
     isLoading: false,
-    posts: []
+    posts: [],
+    error_msg: ''
 }
 
 export function postReducer(state: State = initialState, action: All_Actions): State {
     switch (action.type) {
-        case ActionTypes.LOADING_POSTS:
+        case ActionTypes.LOAD_POSTS:
             return {
                 ...state,
                 isLoading: true
             }
-        case ActionTypes.LOAD_POSTS:
+        case ActionTypes.LOAD_POSTS_SUCCESS:
             return {
                 ...state,
                 isLoading: false,
                 posts: action.payload
+            }
+            case ActionTypes.LOAD_ERROR: 
+            return {
+                ...state,
+                isLoading: false,
+                error_msg: action.payload
             }
     }
 }
